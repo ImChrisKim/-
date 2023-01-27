9eb45947bded6c34e694e8174d22b203311c9df6
SungWooKim <chriskim9512@gmail.com>
2023-01-27 17:34:08 +0900

    .

--- 백준 솔루션_정렬/test.py
+++ 백준 솔루션_정렬/test.py
@@ -1,14 +1,16 @@
 def most_common (arr):
     count_list = []
+    max_value = []
     no_dul_arr = list(set(arr))
     no_dul_arr.sort()
     arr.sort()
-    print(arr)
-    print(no_dul_arr)
     for i in range(len(no_dul_arr)):
         count_list.append(arr.count(no_dul_arr[i]))
-    print(count_list)
-            
-        
-
-most_common([1, 3, 8, -2, 2, 2, 2, 8])
+    for i in range(len(count_list)):
+        if count_list[i] == max(count_list):
+            max_value.append(no_dul_arr[i])
+    if len(max_value) == 1:
+        return max_value[:]
+    else :
+        max_value.sort()
+        return max_value[-2]
